34f4ef5f4a3ba5e52106f3689b5c7011bdda2b91
manish <manishsuriyal21@gmail.com>
2026-06-09 10:26:58 +0530

    syntax error

--- frontend/src/components/OwnerDashboard.jsx
+++ frontend/src/components/OwnerDashboard.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Nav from './NaV.JSX'
+
 import { useSelector } from 'react-redux'
 import { FaUtensils } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
 import { FaPen } from "react-icons/fa";
 import OwnerItemCard from './ownerItemCard';
+import Nav from './Nav';
 function OwnerDashboard() {
   const { myShopData } = useSelector(state => state.owner)
   const navigate = useNavigate()
